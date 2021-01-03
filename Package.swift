1b8a91d6a13fddb3c7a1aa9b92e49f7df701b00d
Dal Rupnik <legoless@gmail.com>
2021-01-03 15:17:43 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Blueprints",
     platforms: [
-        .iOS(.v8),
+        .iOS(.v9),
         .tvOS(.v9),
         .macOS(.v10_11)
     ],
